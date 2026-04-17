fef760387f77aa8229bf6895fa46340969878e80
Andikanajmi Levi Maheswara <52741852+MysticHex@users.noreply.github.com>
2026-04-17 14:06:26 +0700

    Modified dashboard

--- resources/views/dinas/dashboard.blade.php
+++ resources/views/dinas/dashboard.blade.php
@@ -59,10 +59,13 @@
     </nav>
     
     <div class="sidebar-bottom">
-        <a href="#" class="logout-btn">
+        <a href="#" class="logout-btn" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
             <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>
             Keluar
         </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+        </form>
     </div>
 </aside>
 @endsection
